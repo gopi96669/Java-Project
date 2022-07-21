369c2910136e9e76b80fc1f86086e79a02d25082
ramani.gopikrishna <ramani.gopikrishna@HYDPCM595412L.ad.infosys.com>
2022-07-21 13:08:25 +0530

    First Commit

---
+++ src/firstProgram/HelloWorld.java
@@ -0,0 +1,11 @@
+package firstProgram;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("GOPI KRISHNA");
+		// TODO Auto-generated method stub
+
+	}
+
+}
